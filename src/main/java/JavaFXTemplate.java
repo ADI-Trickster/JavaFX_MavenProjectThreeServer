bd27105cc1d4efe6ae348cc1b53eaaa97283e237
ADI_Anaya <anayaadrian121504@gmail.com>
2025-11-17 13:01:43 -0600

    added basic template (need to change)

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -4,6 +4,8 @@ import javafx.animation.RotateTransition;
 import javafx.animation.SequentialTransition;
 import javafx.application.Application;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -24,39 +26,48 @@ public class JavaFXTemplate extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setTitle("Welcome to JavaFX");
-		
-		 Rectangle rect = new Rectangle (100, 40, 100, 100);
-	     rect.setArcHeight(50);
-	     rect.setArcWidth(50);
-	     rect.setFill(Color.VIOLET);
+        try {
+            Parent root = (Parent) FXMLLoader.load(getClass().getResource("serverFXML.fxml"));
+            primaryStage.setTitle("Adrian R. Anaya Homework 4");
 
-	     RotateTransition rt = new RotateTransition(Duration.millis(5000), rect);
-	     rt.setByAngle(270);
-	     rt.setCycleCount(4);
-	     rt.setAutoReverse(true);
-	     SequentialTransition seqTransition = new SequentialTransition (
-	         new PauseTransition(Duration.millis(500)),
-	         rt
-	     );
-	     seqTransition.play();
-	     
-	     FadeTransition ft = new FadeTransition(Duration.millis(5000), rect);
-	     ft.setFromValue(1.0);
-	     ft.setToValue(0.3);
-	     ft.setCycleCount(4);
-	     ft.setAutoReverse(true);
-
-	     ft.play();
-	     BorderPane root = new BorderPane();
-	     root.setCenter(rect);
-	     
-	     Scene scene = new Scene(root, 700,700);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		
-				
-		
+            Scene s1 = new Scene(root, (double)800.0F, (double)800.0F);
+            s1.getStylesheets().add("styles.css");
+            primaryStage.setScene(s1);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+//		primaryStage.setTitle("Welcome to JavaFX");
+//
+//		 Rectangle rect = new Rectangle (100, 40, 100, 100);
+//	     rect.setArcHeight(50);
+//	     rect.setArcWidth(50);
+//	     rect.setFill(Color.VIOLET);
+//
+//	     RotateTransition rt = new RotateTransition(Duration.millis(5000), rect);
+//	     rt.setByAngle(270);
+//	     rt.setCycleCount(4);
+//	     rt.setAutoReverse(true);
+//	     SequentialTransition seqTransition = new SequentialTransition (
+//	         new PauseTransition(Duration.millis(500)),
+//	         rt
+//	     );
+//	     seqTransition.play();
+//
+//	     FadeTransition ft = new FadeTransition(Duration.millis(5000), rect);
+//	     ft.setFromValue(1.0);
+//	     ft.setToValue(0.3);
+//	     ft.setCycleCount(4);
+//	     ft.setAutoReverse(true);
+//
+//	     ft.play();
+//	     BorderPane root = new BorderPane();
+//	     root.setCenter(rect);
+//
+//	     Scene scene = new Scene(root, 700,700);
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
 	}
 
 }
