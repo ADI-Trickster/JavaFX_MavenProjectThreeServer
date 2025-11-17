b9b63d54c0277ec3c61ac3fedc837f874c280ba8
ADI_Anaya <anayaadrian121504@gmail.com>
2025-11-17 13:01:04 -0600

    added controller

---
+++ src/main/java/MyController.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+//import javafx.scene.image.Image;
+
+public class MyController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+
+}
