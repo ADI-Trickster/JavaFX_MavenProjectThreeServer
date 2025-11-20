a5831a70f6364bc3987b93791945fef6b1f81a9a
ADI_Anaya <anayaadrian121504@gmail.com>
2025-11-20 06:39:08 -0600

    for data transfer

---
+++ src/main/java/Cards.java
@@ -0,0 +1,18 @@
+public class Cards {
+    private char suit;
+    private int value;
+    private String cardImage;
+
+    public Cards(char suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public char getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/PokerInfo.java
@@ -0,0 +1,64 @@
+//import javax.smartcardio.Card;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+class PokerInfo implements Serializable {
+
+    private ArrayList<Cards> playerCards;
+    private ArrayList<Cards> dealerCards;
+    private int potAmount;
+    private int playerBet;
+    private int bigBlind;
+    private int pairPlusBet;
+    private boolean playerFolded;
+    private int totalWinnings;
+    private int gameOver;
+
+    private String message;
+
+    public PokerInfo(){
+        playerCards = new ArrayList<>();
+        dealerCards = new ArrayList<>();
+    }
+
+    public ArrayList<Cards> getPlayerHand() { return playerCards; }
+    public void setPlayerHand(ArrayList<Cards> playerHand) {
+        this.playerCards = playerHand;
+    }
+
+    public ArrayList<Cards> getDealerHand() { return dealerCards; }
+    public void setDealerHand(ArrayList<Cards> dealerHand) {
+        this.dealerCards = dealerHand;
+    }
+
+    public int getPlayerBet() { return playerBet; }
+    public void setPlayerBet(int playerBet) { this.playerBet = playerBet; }
+
+    public boolean isPlayerFolded() { return playerFolded; }
+    public void setPlayerFolded(boolean playerFolded) { this.playerFolded = playerFolded; }
+
+    public int getBigBlind() { return bigBlind; }
+    public void setBigBlind(int bigBlind) { this.bigBlind = bigBlind; }
+
+    public void addCardToHand(Cards card) {
+        playerCards.add(card);
+    }
+
+    public void clearPlayerCards() {
+        playerCards.clear();
+    }
+
+    public void addDealerCard(Cards card) {
+        dealerCards.add(card);
+    }
+    public void clearDealerCards() {
+        dealerCards.clear();
+    }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+
+
+
+}
